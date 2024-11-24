1989abd7d97ecedd19fd9388df110d49a549c7f7
nn4766189 <hackehacke73@gmail.com>
2024-11-25 03:54:42 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -6,8 +6,6 @@ import subprocess
 import requests
 import datetime
 import os
-from flask import Flask
-app = Flask(__name__)
 # insert your Telegram bot token here
 bot = telebot.TeleBot('7888734181:AAExm8pfH6RmETXzxK-nXpP893WT3YNm7gA')
 
